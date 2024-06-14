072d6116eb37443231b97f75b3655e3eb1f32ae7
skrsumit250 <skrsumit250@gmail.com>
2024-06-14 12:39:59 +0530

    update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.get('/print',(req,res)=>{
     res.json({message:"server is running"});
+    console.log("server");
 });
 app.listen(PORT,()=>{
     console.log("Server is running");
